67b073ea57dfd8b12412798608af77896bb4e8e9
Pavel <dabdab@mail.ru>
2022-10-16 21:06:08 +0300

    Second number in three-digit number

---
+++ Test1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine ("Введите трехзначное число: ");
+
+int number = int.Parse(Console.ReadLine());
+
+
+int SecondN = (number % 100 - number % 10) / 10;
+
+Console.WriteLine(SecondN);

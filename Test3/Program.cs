f29e1a8244536240dff5de8859e5c05a346e69e3
Pavel <dabdab@mail.ru>
2022-10-17 08:57:11 +0300

    Day off or not

---
+++ Test3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число соответстующий дню недели: ");
+int num = int.Parse(Console.ReadLine());
+switch (num)
+{
+    case 1: Console.WriteLine("Нет, это не выходной"); break;
+    case 2: Console.WriteLine("Нет, это не выходной"); break;
+    case 3: Console.WriteLine("Нет, это не выходной"); break;
+    case 4: Console.WriteLine("Нет, это не выходной"); break;
+    case 5: Console.WriteLine("Нет, это не выходной"); break;
+    case 6: Console.WriteLine("Да, это не выходной"); break;
+    case 7: Console.WriteLine("Да, это не выходной"); break;
+    default: Console.WriteLine("ERROR"); break;
+}
+Console.ReadLine();

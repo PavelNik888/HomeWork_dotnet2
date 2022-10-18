0f51aab2c8cfd117ff67f51256fe9135d62fc9f5
Pavel <dabdab@mail.ru>
2022-10-18 08:58:26 +0300

    Нахождение третьей цифры в числе

---
+++ Test2/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine ("Введите число: ");
+
+int number = int.Parse(Console.ReadLine());
+
+if (number < 100)
+{
+    Console.WriteLine("третий цифры нет!")
+}
+else
+{
+    int coef = 10;
+
+    if (number > 999)
+    {
+    while (true)
+    {
+        int third = number / coef;
+        if (third > 100 && third < 1000)
+        {
+            break;
+        }
+        else 
+        {
+            coef *= 10;
+        }
+    }
+    Console.WriteLine($"The third number is {(n / coef) % 10}");
+    }
+    else
+    {
+     Console.WriteLine($"The third number is {n % 10}");   
+    }
+}
